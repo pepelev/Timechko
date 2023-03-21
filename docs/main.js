05c73574405dcf9996061561e10b74b0ff3300ed
pepelev <kran440@gmail.com>
2023-03-21 12:14:53 +0500

    v2

--- docs/main.js
+++ docs/main.js
@@ -10,45 +10,121 @@ const config = getConfig();
 const exports = await getAssemblyExports(config.mainAssemblyName);
 
 // Access JSExport methods using exports.<Namespace>.<Type>.<Method>
-const now = exports.Time.Now;
-const parse = exports.Time.Parse;
+const nowRaw = exports.Time.Now;
+const parseRaw = exports.Time.Parse;
 
-const key = "data";
-const read = function () {
-    try {
-        return JSON.parse(
-            window.localStorage.getItem(key)
-        ) || [];
-    } catch {
-        return [];
+const parse = function (value, type) {
+    const parsed = parseRaw(value, type);
+    return JSON.parse(parsed);
+};
+
+const queriesStorage = {
+    /*
+    * v1 storage format is array of
+    * {
+    *   "index":0,
+    *   "value":{
+    *     "type":"DateTime",
+    *     "dateTime":"2023-03-18T12:32:21Z",
+    *     "unixTime":"1679142741",
+    *     "ticks":"638147395410000000"
+    *   }
+    * }
+    *
+    * v2 storage format is array of
+    * {
+    *   "type": "DateTime",
+    *   "value": "2023-03-18T12:32:21Z"
+    * }
+    */
+    key: "data",
+    fromV1Storage: function (element) {
+        const value = element.value;
+        const input = getField(value, value.type);
+        return parse(input, value.type);
+    },
+    read: function () {
+        try {
+            return (JSON.parse(
+                window.localStorage.getItem(this.key)
+            ) || [])
+                .map(
+                    (element, index) => ({
+                        index: index,
+                        origin: "local-storage",
+                        value: "index" in element
+                            ? this.fromV1Storage(element) // v1 format
+                            : parse(element.value, element.type) // v2 format
+                    })
+                ).filter(x => !!x);
+        } catch {
+            return [];
+        }
+    },
+    write: function (queries) {
+        const data = JSON.stringify(
+            queries.map(query => ({ type: query.value.type, value: getField(query.value, query.value.type) }))
+        );
+        try {
+            window.localStorage.setItem(this.key, data);
+        } catch {
+            // suppress any errors
+        }
     }
-}
+};
 
-const store = function (parsedQueries) {
-    const data = JSON.stringify(
-        parsedQueries.map(item => ({ index: item.index, value: item.value }))
-    );
-    try {
-        window.localStorage.setItem(key, data);
-    } catch {
-        // suppress any errors
-    }
-}
+const getField = function (obj, type) {
+    const fieldMap = {
+        DateTime: "dateTime",
+        UnixTime: "unixTime",
+        UnixTimeSeconds: "unixTimeSeconds",
+        UnixTimeMilliseconds: "unixTimeMilliseconds",
+        UnixTimeMicroseconds: "unixTimeMicroseconds",
+        Ticks: "ticks",
+        TimeGuid: "timeGuid"
+    };
+    const name = fieldMap[type];
+    return obj[name];
+};
 
-const storedEntries = read();
-let index = 0;
-for (let i = 0; i < storedEntries.length; i++) {
-    index = Math.max(index, storedEntries[i].index + 1);
-}
+
+
+const readQueries = queriesStorage.read();
+
+const hashParts = document.location.hash
+    ? decodeURI(document.location.hash.slice(1)).split("@")
+    : [];
+
+const knownTypes = [
+    "Guess",
+    "DateTime",
+    "UnixTime (seconds)",
+    "UnixTime (milliseconds)",
+    "UnixTime (microseconds)",
+    "UnixTime (guess)",
+    "Ticks",
+    "TimeGuid"
+];
+
+const startInput = hashParts[0] || nowRaw();
+const startType = hashParts[1] && knownTypes.find(type => type === hashParts[1]) || "Guess";
 
 window.app = new Vue({
     el: '#app',
     data: {
         state: "Typing",
-        index: index,
-        input: document.location.hash ? decodeURI(document.location.hash.slice(1)) : now(),
-        type: "Guess",
-        parsedQueries: storedEntries.map(item => ({ index: item.index, origin: "local-storage", value: item.value })),
+        index: readQueries.length,
+        input: startInput,
+        type: startType,
+        columns: {
+            dateTime: true,
+            unixTimeSeconds: true,
+            unixTimeMilliseconds: false,
+            unixTimeMicroseconds: false,
+            ticks: true,
+            timeGuid: false
+        },
+        parsedQueries: readQueries,
         flushFailed: false
     },
     methods: {
@@ -57,7 +133,7 @@ window.app = new Vue({
                 this.parsedQueries.unshift(
                     { index: this.index, origin: "input", value: this.parsed }
                 );
-                this.index++
+                this.index++;
             } else {
                 this.state = "FlushFailed";
             }
@@ -67,12 +143,15 @@ window.app = new Vue({
         },
         flushFailureIndicated: function () {
             this.state = "Typing";
+        },
+        hasParsedQueryWithType: function (type) {
+            return (this.parsed && this.parsed.type === type) ||
+                !!this.parsedQueries.find(query => query.value.type === type);
         }
     },
     computed: {
         parsed: function() {
-            const value = parse(this.input, this.type);
-            return JSON.parse(value);
+            return parse(this.input, this.csharpType);
         },
         inputClass: function () {
             if (this.input.trim() === '') {
@@ -84,18 +163,54 @@ window.app = new Vue({
             } else {
                 return "input-not-parsed";
             }
+        },
+        shownColumns: function () {
+            return {
+                dateTime:             this.columns.dateTime             || this.csharpType === "DateTime"             || this.hasParsedQueryWithType("DateTime"),
+                unixTimeSeconds:      this.columns.unixTimeSeconds      || this.csharpType === "UnixTimeSeconds"      || this.hasParsedQueryWithType("UnixTimeSeconds"),
+                unixTimeMilliseconds: this.columns.unixTimeMilliseconds || this.csharpType === "UnixTimeMilliseconds" || this.hasParsedQueryWithType("UnixTimeMilliseconds"),
+                unixTimeMicroseconds: this.columns.unixTimeMicroseconds || this.csharpType === "UnixTimeMicroseconds" || this.hasParsedQueryWithType("UnixTimeMicroseconds"),
+                ticks:                this.columns.ticks                || this.csharpType === "Ticks"                || this.hasParsedQueryWithType("Ticks"),
+                timeGuid:             this.columns.timeGuid             || this.csharpType === "TimeGuid"             || this.hasParsedQueryWithType("TimeGuid")
+            };
+        },
+        csharpType: function () {
+            const type = this.type.trim();
+            if (type === "UnixTime (seconds)") {
+                return "UnixTimeSeconds";
+            } else if (type === "UnixTime (milliseconds)") {
+                return "UnixTimeMilliseconds";
+            } else if (type === "UnixTime (microseconds)") {
+                return "UnixTimeMicroseconds";
+            } else if (type === "UnixTime (guess)") {
+                return "UnixTimeGuess";
+            } else {
+                return type;
+            }
+        },
+        hash: function () {
+            const type = this.type.trim();
+            const input = this.input.trim();
+            return type === "Guess"
+                ? input
+                : `${input}@${type}`;
         }
     },
     watch: {
-        input: function () {
-            document.location.hash = this.input;
+        hash: function () {
+            try {
+                document.location.hash = this.hash;
+            }
+            catch {
+                // suppress any errors
+            }
             this.state = "Typing";
         },
         parsedQueries: function () {
-            store(this.parsedQueries);
+            queriesStorage.write(this.parsedQueries);
         }
     }
-})
+});
 
-document.getElementById("root").className = "loaded"
+document.getElementById("root").className = "loaded";
 document.getElementsByClassName("input")[0].focus();
